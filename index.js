bff58a87cebdaa481187003539df051b0c073229
Raztsook <95634001+Raztsook@users.noreply.github.com>
2025-05-01 23:05:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: new,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
