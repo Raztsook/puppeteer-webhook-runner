a075220e808bd6df26ed21e09992f5aa296a6193
Raztsook <95634001+Raztsook@users.noreply.github.com>
2025-05-01 13:14:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.get("/", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log("Listening on port", port));
